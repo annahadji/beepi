10b4cd854c9cba4f478477dff707a24c51d65cbe
Anna Hadjitofi <annahadji@users.noreply.github.com>
2021-07-16 02:32:44 +0100

    Forma and lint.

--- record_picam.py
+++ record_picam.py
@@ -1,6 +1,5 @@
 """Small script saving video recordings using Picamera."""
 import argparse
-import ctypes as ct
 import datetime
 import logging
 import math
@@ -8,10 +7,8 @@ import pathlib
 import shutil
 import subprocess
 import time
-from typing import Generator, Dict, Any
+from typing import Dict, Any
 
-import picamera
-from picamera import mmal
 import brightpi
 
 logger = logging.getLogger("BeePi")
@@ -27,54 +24,67 @@ WRITE_TO_USB_AFTER = 8.0  # Try to write to usb after accumulating this many gb 
 LEAVE_SPARE_ON_PI = 6.0  # Make sure to leave this many spare gb on filesystem
 BYTES_PER_GB = 1024 * 1024 * 1024
 
-def record_n_segments(n: int, seconds: int, iteration: int) -> None:
-    """Record n segments of footage of a particular duration (in seconds).
-    Iteration parameter is used in naming."""
+
+def record_n_segments(num_segs: int, seconds: int, name_prefix: str) -> None:
+    """Record n segments of footage of a particular duration (in seconds)."""
     start_rec = pathlib.Path("/home/pi/picam/hooks/start_record")
     stop_rec = pathlib.Path("/home/pi/picam/hooks/stop_record")
-    for segment in range(n):
-        filename = f"{iteration}-{segment}-{datetime.datetime.now().strftime('%d%m%y-%H%M')}.ts"
-        with start_rec.open("w") as f:
-            f.write(f"filename={filename}")
+    for segment in range(num_segs):
+        filename = f"{name_prefix}-{segment}-{datetime.datetime.now().strftime('%d%m%y-%H%M')}.ts"
+        with start_rec.open("w") as file:
+            file.write(f"filename={filename}")
         time.sleep(seconds)
         stop_rec.touch()
         logger.info("Video saved, %s.", filename)
         time.sleep(1)
-        
-def convert_to_mp4(
-    ts_file, remove_orig: bool = False
-) -> None:
-    """Convert videos in a directory to grayscale. Converted videos will be saved
-    to same path with 'gry-' prefix and original videos can optionally be removed.
+
+
+def convert_to_mp4(ts_file: pathlib.Path, remove_orig: bool = False) -> None:
+    """Convert videos in a directory to mp4. Converted videos will be saved
+    and original videos can optionally be removed.
 
     Exceptions raised:
         CalledProcessError: raised if ffmpeg doesnt successfully convert a file.
     """
     new_path = ts_file.parent / (ts_file.stem + ".mp4")
     subprocess.run(
-        ["ffmpeg", "-i", ts_file, "-c:v", "copy", "-c:a", "copy", "-bsf:a", "aac_adtstoasc", str(new_path)], check=True
+        [
+            "ffmpeg",
+            "-i",
+            ts_file,
+            "-c:v",
+            "copy",
+            "-c:a",
+            "copy",
+            "-bsf:a",
+            "aac_adtstoasc",
+            str(new_path),
+        ],
+        check=True,
     )
-    logger.info("Converted and saved %s.", str(ts_file))
+    logger.info("Converted to mp4 and saved %s.", str(ts_file))
     if remove_orig:
         ts_file.unlink()
         logger.info("Deleted %s.", str(ts_file))
 
 
-def write_to_usb(data_path: pathlib.Path, usb_path: pathlib.Path) -> None:
-    """Write all .h264 video files in data dir to path on usb, and remove original files."""
+def write_to_usb(data_path: pathlib.Path, usbstick_path: pathlib.Path) -> None:
+    """Write all .mp4 video files in data dir to path on usb, and remove original files."""
     for video_path in data_path.glob("*.mp4"):
-        target_path = usb_path / "data" / video_path.name
+        target_path = usbstick_path / "data" / video_path.name
         target_path.parent.mkdir(parents=True, exist_ok=True)
         shutil.copyfile(video_path, target_path)
         video_path.unlink()
 
-def test_setup(args: Dict[str, Any]):
+
+def test_setup(arguments: Dict[str, Any]):
     """Change arguments to test the setup."""
-    args["experiment_name"] = "test"
-    args["segment_length"] = 3  # seconds
-    args["session_length"] = 7  # seconds
-    args["fps"] = 60
-    args["ir"] = True
+    arguments["experiment_name"] = "test"
+    arguments["segment_length"] = 3  # seconds
+    arguments["session_length"] = 7  # seconds
+    arguments["fps"] = 60
+    arguments["ir"] = True
+
 
 if __name__ == "__main__":
 
@@ -121,7 +131,7 @@ if __name__ == "__main__":
     # Setup test arguments if debug
     if args["debug"]:
         test_setup(args)
-    assert (args["segment_length"] < args["session_length"]), "Seg len > session len."
+    assert args["segment_length"] < args["session_length"], "Seg len > session len."
     logger.info("Running with args: %s", str(args))
     # ------------------------
     # Set up lighting conditions
@@ -137,9 +147,10 @@ if __name__ == "__main__":
         logger.info("BrightPi setup, IR leds ON: %s", str(leds_indices_on))
     # ------------------------
     # Recording loop
-    n_segments = 5
+    N_SEGMENTS = 5
     num_iterations = max(
-        1, int(math.ceil(args["session_length"] / (n_segments * args["segment_length"])))
+        1,
+        int(math.ceil(args["session_length"] / (N_SEGMENTS * args["segment_length"]))),
     )
     space_on_usb = True
     for recording_iter in range(num_iterations):
@@ -147,20 +158,33 @@ if __name__ == "__main__":
         logger.info("Recording loop %d / %d.", recording_iter + 1, num_iterations)
         # ---
         # Record n segments of footage for certain duration
-        camera_proc = subprocess.Popen(["./picam", "--alsadev", "hw:1,0",
-                                        "--width", "640",
-                                        "--height", "480",
-                                        "--fps", "60",
-                                        "--mode", "6",
-                                        "--hflip",
-                                        "--wb", "greyworld",
-                                        "--iso", "800"], cwd="/home/pi/picam")
+        camera_proc = subprocess.Popen(
+            [
+                "./picam",
+                "--alsadev",
+                "hw:1,0",
+                "--width",
+                "640",
+                "--height",
+                "480",
+                "--fps",
+                f"{args['fps']}",
+                "--mode",
+                "6",
+                "--hflip",
+                "--wb",
+                "greyworld",
+                "--iso",
+                "800",
+            ],
+            cwd="/home/pi/picam",
+        )
         time.sleep(2)  # Camera warmup
-        logger.info("About to start recording %d segments...", n_segments)
+        logger.info("About to start recording %d segments...", N_SEGMENTS)
         record_n_segments(
-            n=n_segments,
+            num_segs=N_SEGMENTS,
             seconds=args["segment_length"],
-            iteration=recording_iter  # Passed for naming
+            name_prefix=f"{args['experiment_name']}-{recording_iter}"  # Passed for naming
         )
         logger.info("Recording finished.")
         camera_proc.terminate()
@@ -169,9 +193,9 @@ if __name__ == "__main__":
         # Convert from .ts to .mp4
         ts_files_save_archive_path = pathlib.Path("/home/pi/picam/archive")
         ts_files_save_rec_path = pathlib.Path("/home/pi/picam/rec")
-        for filename in ts_files_save_archive_path.glob("*.ts"):
-            convert_to_mp4(filename, remove_orig=True)
-            (ts_files_save_rec_path / filename.name).unlink()
+        for ts_filename in ts_files_save_archive_path.glob("*.ts"):
+            convert_to_mp4(ts_filename, remove_orig=True)
+            (ts_files_save_rec_path / ts_filename.name).unlink()
         # ---
         # Reset lighting conditions
         if args["ir"]:
@@ -180,10 +204,12 @@ if __name__ == "__main__":
             logger.info("BrightPi reset, IR leds ON: %s", str(leds_indices_on))
         # ---
         # Check available space in filesystem
-        space = shutil.disk_usage(ts_files_save_archive_path)  # Returns total, used and free bytes
+        space = shutil.disk_usage(
+            ts_files_save_archive_path
+        )  # Returns total, used and free bytes
         total = float(space.total) / BYTES_PER_GB
         used = float(space.used) / BYTES_PER_GB
-        logger.info("Filesystem usage %.2fgb / %.2fgb." % (used, total))
+        logger.info("Filesystem usage %dgb / %dgb.", used, total)
         if used > WRITE_TO_USB_AFTER and space_on_usb:
             # ---
             # Check available space in USB
@@ -192,19 +218,25 @@ if __name__ == "__main__":
             usb_total = float(usb_space.total) / BYTES_PER_GB
             usb_used = float(usb_space.used) / BYTES_PER_GB
             usb_free = float(usb_space.free) / BYTES_PER_GB
-            logger.info("USB usage %.2fgb / %.2fgb." % (usb_used, usb_total))
+            logger.info("USB usage %dgb / %dgb.", round(usb_used), round(usb_total))
             if used > usb_free:
                 space_on_usb = False
-                logger.info("USB storage will be exceeded by next write. Next write cancelled.")
+                logger.info("USB space will be exceeded. Next write cancelled.")
                 break
             # ---
             # Write to USB
-            logger.info("Moving .mp4 files in %s to %s...", str(ts_files_save_archive_path), str(usb_path))
+            logger.info(
+                "Moving .mp4 files in %s to %s...",
+                str(ts_files_save_archive_path),
+                str(usb_path),
+            )
             write_to_usb(ts_files_save_archive_path, usb_path)
-            used = float(shutil.disk_usage(ts_files_save_archive_path).used) / BYTES_PER_GB
-            logger.info("Files moved. Filesystem usage now: %.2fgb." % (used))
+            used = (
+                float(shutil.disk_usage(ts_files_save_archive_path).used) / BYTES_PER_GB
+            )
+            logger.info("Files moved. Filesystem usage now: %dgb.", round(used))
         # ---
         # Break out of loop if space is exceeded
         if (total - used) < LEAVE_SPARE_ON_PI:
-            logger.info("Terminating program at loop %d due to space constraints.", recording_iter)
+            logger.info("Terminating at loop %d due to space.", recording_iter)
             break
